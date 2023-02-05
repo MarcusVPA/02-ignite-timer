932d3cca0d8574dbcf994d03c51ae2516c5862ef
MarcusVPA <marcusmayme18@gmail.com>
2023-02-05 02:54:44 -0300

    finished project

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,20 @@
-import { HeaderContainer } from "./styles";
-import { NavLink } from 'react-router-dom';
-import { Timer, Scroll } from 'phosphor-react';
-import logoIgnite from '../../assets/logo-ignite.svg';
+import { HeaderContainer } from './styles'
+import { NavLink } from 'react-router-dom'
+import { Timer, Scroll } from 'phosphor-react'
+import logoIgnite from '../../assets/logo-ignite.svg'
 
 export function Header() {
-    return (
-        <HeaderContainer>
-            <img src={logoIgnite} alt="" />
-            <nav>
-                <NavLink to="/" title="Timer">
-                    <Timer size={24} />
-                </NavLink>
-                <NavLink to="/history" title="Histórico">
-                    <Scroll size={24} />
-                </NavLink>
-            </nav>
-        </HeaderContainer>
-    );
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,36 +1,67 @@
-import { Play } from 'phosphor-react';
-import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from './styles';
+import { HandPalm, Play } from 'phosphor-react'
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from './styles'
+import * as zod from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useContext } from 'react'
+import { useForm } from 'react-hook-form';
+import { Countdown } from './components/Countdowm'
+import { NewCycleForm } from './components/NewCycleForm';
+import { FormProvider } from 'react-hook-form';
+import { CyclesContext } from '../../contexts/CyclesContext';
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informa a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(1, 'O ciclo precisa ser de no mínimo de 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo de 60 minutos'),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-    return (
-        <HomeContainer>
-            <form action="">
-                <FormContainer>
-                    <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput 
-                        id="taks" 
-                        placeholder="Dê um nome para o seu projeto" 
-                    />
-                    <label htmlFor="minutesAmount">durante</label>
-                    <MinutesAmountInput 
-                        type="number" 
-                        id="minutesAmount" 
-                        placeholder="00" 
-                    />
-                    <span>minutos.</span>
-                </FormContainer>
-                <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
-                    <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
-                </CountdownContainer>
-                <StartCountdownButton type="submit">
-                    <Play size={24}/>
-                    Começar
-                </StartCountdownButton>
-            </form>
-        </HomeContainer>
-    );
+  const { activeCycle, createNewCycle, interruptCurrentCycle } = useContext(CyclesContext);
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
+  });
+
+  const { handleSubmit, watch, reset } = newCycleForm;
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data);
+    reset();
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+          <FormProvider {...newCycleForm}>
+            <NewCycleForm />
+          </FormProvider>
+          <Countdown />
+        {activeCycle ? (
+          <StopCountdownButton onClick={interruptCurrentCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
+      </form>
+    </HomeContainer>
+  )
 }
