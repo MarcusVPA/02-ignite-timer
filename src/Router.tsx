ecae6ccf71af7bc806bb0baf0d6ec5dc8cdd8e58
MarcusVPA <marcusmayme18@gmail.com>
2023-02-10 23:45:14 -0300

    add uri in header component and change uri in router

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,9 +6,9 @@ import { Home } from './pages/Home'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
+      <Route path="/02-ignite-timer" element={<DefaultLayout />}>
+        <Route path="/02-ignite-timer" element={<Home />} />
+        <Route path="/02-ignite-timer/history" element={<History />} />
       </Route>
     </Routes>
   )
